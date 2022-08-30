ebc8512a4034c2491717a57a282072524ec543dc
Ulises Rojas González <ulises.rogo@gmail.com>
2022-08-30 13:33:54 -0500

    52-Adding-form-inputs

--- expenses-project/src/App.js
+++ expenses-project/src/App.js
@@ -1,3 +1,4 @@
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -30,10 +31,10 @@ const App = () => {
 
     return (
         <div>
-            <h2>Let's get started!</h2>
+            <NewExpense />
             <Expenses items={expenses} />
         </div>
     );
-}
+};
 
 export default App;
