53e486c724dbde0f44a0d60de5d8bf313d5f97ee
Ulises Rojas González <ulises.rogo@gmail.com>
2022-08-30 16:24:35 -0500

    56-Updating-State-That-Depends-On-The-Previous-State

--- expenses-project/src/components/NewExpense/ExpenseForm.js
+++ expenses-project/src/components/NewExpense/ExpenseForm.js
@@ -6,27 +6,24 @@ const ExpenseForm = () => {
     const [userInput, setUserInput] = useState({
         enteredTitle: '',
         enteredAmount: '',
-        enteredDate: ''
-    })
+        enteredDate: '',
+    });
 
     const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value
+        setUserInput((prevState) => {
+            return { ...prevState, enteredTitle: event.target.value };
         });
     };
 
     const amountChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value
+        setUserInput((prevState) => {
+            return { ...prevState, enteredAmount: event.target.value };
         });
     };
 
     const dateChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
+        setUserInput((prevState) => {
+            return { ...prevState, enteredDate: event.target.value };
         });
     };
 
