294c37982f1e35b0ba76a65aa05fc8cd8391667e
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-01 17:50:23 -0500

    68-Adding-Conditional-Return-Statements

--- expenses-project/src/components/Expenses/Expenses.js
+++ expenses-project/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
-import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -17,19 +17,6 @@ const Expenses = (props) => {
         return enteredYear === expenseYear;
     });
 
-    let expensesContent = <p>No expenses found</p>;
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.item}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ));
-    }
-
     return (
         <div>
             <Card className="expenses">
@@ -37,7 +24,7 @@ const Expenses = (props) => {
                     onFilterYear={filterChangeHandler}
                     enteredYear={enteredYear}
                 />
-                {expensesContent}
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     );
