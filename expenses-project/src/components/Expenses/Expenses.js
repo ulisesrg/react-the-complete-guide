3510d537cb6f761d882eabf66a203b4e305924e9
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-02 14:42:38 -0500

    71-Wrap-Up-and-Next-Steps

--- expenses-project/src/components/Expenses/Expenses.js
+++ expenses-project/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -24,6 +25,7 @@ const Expenses = (props) => {
                     onFilterYear={filterChangeHandler}
                     enteredYear={enteredYear}
                 />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
