ffcb9d5d525e64d535c4ec906ef870a71ebd3ac8
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-05 13:47:10 -0500

    90-Adding-a-User-Component

---
+++ users-project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AddUser from './components/users/AddUser';
+
+function App() {
+    return (
+        <div>
+            <AddUser />
+        </div>
+    );
+}
+
+export default App;
