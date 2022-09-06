67cb40f893a0eb7aecc5665a116511b38dfe30df
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-06 11:39:00 -0500

    95-Adding-a-Users-List-Component

--- users-project/src/App.js
+++ users-project/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import AddUser from './components/users/AddUser';
+import UsersList from './components/users/UsersList';
 
 function App() {
     return (
         <div>
             <AddUser />
+            <UsersList users={[]} />
         </div>
     );
 }
