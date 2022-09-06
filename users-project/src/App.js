8402d94a62dda0e4d8ef5e26ba38c622ff48516b
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-06 11:53:23 -0500

    96-Managing-a-List-Of-Users-via-State

--- users-project/src/App.js
+++ users-project/src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from './components/users/AddUser';
 import UsersList from './components/users/UsersList';
 
 function App() {
+    const [usersList, setUsersList] = useState([]);
+
+    const addUserHandler = (userName, userAge) => {
+        setUsersList((prevUsersList) => {
+            return [
+                ...prevUsersList,
+                { user: userName, age: userAge, id: Math.random().toString() },
+            ];
+        });
+    };
+
     return (
         <div>
-            <AddUser />
-            <UsersList users={[]} />
+            <AddUser onAddUser={addUserHandler} />
+            <UsersList users={usersList} />
         </div>
     );
 }
