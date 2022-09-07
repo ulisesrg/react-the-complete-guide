3e1a63382969d0982866b027ec7507c3d0e603d6
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-07 17:42:43 -0500

    125-Building-and-Using-a-Custom-Context-Provider-Component

--- 4-login-project/src/components/Login/Login.js
+++ 4-login-project/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
     if (action.type === 'USER_INPUT') {
@@ -49,6 +50,8 @@ const Login = (props) => {
         value: '',
         isValid: null,
     });
+
+    const authCtx = useContext(AuthContext);
 
     // --------------------------- USE EFFECT SUMMARY STARTS ------------------------------- //
     // NO SECOND ARGUMENT
@@ -127,7 +130,7 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, passwordState.value);
+        authCtx.onLogin(emailState.value, passwordState.value);
     };
 
     return (
--- 4-login-project/src/index.js
+++ 4-login-project/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+import { AuthContextProvider } from './store/auth-context';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <AuthContextProvider>
+        <App />
+    </AuthContextProvider>
+);
