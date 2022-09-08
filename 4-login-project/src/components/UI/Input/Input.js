88fce9052478e285c6630c1421e4443ed4105d0f
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-08 13:15:29 -0500

    129-Diving-into-Forward-Refs

--- 4-login-project/src/components/UI/Input/Input.js
+++ 4-login-project/src/components/UI/Input/Input.js
@@ -1,6 +1,26 @@
+import React, { useImperativeHandle, useRef } from 'react';
+
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+    const inputRef = useRef();
+
+    const activate = () => {
+        inputRef.current.focus();
+    };
+
+    /*
+        This hook is used to pass a ref from Login externally to Input component,
+        all through the ref parameter that is a "prop" in Login.js
+        since the last one is a React Component and not a DOM element.
+        React Components can't have refs, so that's the reason why.
+    */
+    useImperativeHandle(ref, () => {
+        return {
+            focus: activate,
+        };
+    });
+
     return (
         <div
             className={`${classes.control} ${
@@ -9,6 +29,12 @@ const Input = (props) => {
         >
             <label htmlFor={props.id}>{props.label}</label>
             <input
+                /* 
+                // BASED ON YOUTUBE VIDEO https://www.youtube.com/watch?v=gwFfzIaKnAU THIS TOTALLY WORKS TOO:
+                ref={ref} // (the parameter)
+                and we wouldn't need inputRef, activate and useImperativeHandler in this file
+                */
+                ref={inputRef}
                 type={props.type}
                 id={props.id}
                 value={props.value}
@@ -17,6 +43,6 @@ const Input = (props) => {
             />
         </div>
     );
-};
+});
 
 export default Input;
