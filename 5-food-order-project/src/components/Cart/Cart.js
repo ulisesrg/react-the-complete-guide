d12f9f3099f80357ee0b57eec026451262f1d743
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-09 12:50:31 -0500

    141-Managing-Cart-and-Modal-State

--- 5-food-order-project/src/components/Cart/Cart.js
+++ 5-food-order-project/src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
-const Cart = () => {
+const Cart = (props) => {
     const cartItems = (
         <ul className={classes['cart-items']}>
             {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(
@@ -13,14 +13,14 @@ const Cart = () => {
     );
 
     return (
-        <Modal>
+        <Modal onClose={props.onClose} >
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button-alt']}>Close</button>
+                <button className={classes['button-alt']} onClick={props.onClose} >Close</button>
                 <button className={classes.button}>Order</button>
             </div>
         </Modal>
