3d2e5620e5983247476c70d42723f3372315860f
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-09 13:53:22 -0500

    144-Adding-a-Cart-Reducer

--- 5-food-order-project/src/components/Layout/HeaderCartButton.js
+++ 5-food-order-project/src/components/Layout/HeaderCartButton.js
@@ -7,17 +7,20 @@ import classes from './HeaderCartButton.module.css';
 const HeaderCartButton = (props) => {
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((previousValue, currentValue) => {
-        return previousValue + currentValue.amount;
-    })
+    const numberOfCartItems = cartCtx.items.reduce(
+        (previousValue, currentValue) => {
+            return previousValue + currentValue.amount;
+        },
+        0
+    );
 
     return (
-        <button className={classes.button} onClick={props.onClick} >
+        <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );
 };
