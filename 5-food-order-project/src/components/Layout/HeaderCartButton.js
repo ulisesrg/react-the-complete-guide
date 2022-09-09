768ec6dd2c3ae29b7965e9d8a40ad5bb4fe586a8
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-09 13:32:14 -0500

    143-Using-the-Context

--- 5-food-order-project/src/components/Layout/HeaderCartButton.js
+++ 5-food-order-project/src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,16 @@
+import { useContext } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((previousValue, currentValue) => {
+        return previousValue + currentValue.amount;
+    })
+
     return (
         <button className={classes.button} onClick={props.onClick} >
             <span className={classes.icon}>
