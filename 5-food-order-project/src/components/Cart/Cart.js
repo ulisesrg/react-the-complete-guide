7cea3906a00a50b58ae0d9c86c8e39be096f6a91
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-12 11:52:38 -0500

    148-Making-Items-Removable

--- 5-food-order-project/src/components/Cart/Cart.js
+++ 5-food-order-project/src/components/Cart/Cart.js
@@ -11,8 +11,13 @@ const Cart = (props) => {
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
-    const cartItemRemoveItemHandler = () => {};
-    const cartItemAddHandler = () => {};
+    const cartItemRemoveItemHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({ ...item, amount: 1 });
+    };
 
     const cartItems = (
         <ul className={classes['cart-items']}>
