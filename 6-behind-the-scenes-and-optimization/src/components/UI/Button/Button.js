ab2999b976c3e00169ec116d6573e7aa300242d4
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-12 14:30:18 -0500

    156-Preventing-Function-Re-Creation-with-useCallback

--- 6-behind-the-scenes-and-optimization/src/components/UI/Button/Button.js
+++ 6-behind-the-scenes-and-optimization/src/components/UI/Button/Button.js
@@ -22,6 +22,10 @@ this tells React to only re-evalute the component if the props changed
 as props.onClick === props.previous.onClick will be false because
 onClick is a function and, hence, an object and two objects can't be the same thing
 So, it will re-evaluate it
+
+Update: With useCallback hook, React will assign the same pointer in memory for
+props.onClick and props.previous.onClick, so it will check that both are the same.
+So, it won't re-evaluate it
 */
 
 export default React.memo(Button);
