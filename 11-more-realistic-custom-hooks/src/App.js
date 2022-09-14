3af4a28f961280e7173f6a85697e610ef0779285
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-14 17:31:10 -0500

    194-Adjusting-the-Custom-Hook-Logic
    This commit adds a different solution without using useMemo and
    useCallback since it was a little more complex than the current
    solution.

--- 11-more-realistic-custom-hooks/src/App.js
+++ 11-more-realistic-custom-hooks/src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -7,35 +7,25 @@ import useHttp from './hooks/use-http';
 function App() {
     const [tasks, setTasks] = useState([]);
 
-    const transformTasks = useCallback((tasksObj) => {
-        const loadedTasks = [];
+    const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-        for (const taskKey in tasksObj) {
-            loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-        }
-
-        setTasks(loadedTasks);
-    }, []);
-
-    const {
-        isLoading,
-        error,
-        sendRequest: fetchTasks,
-    } = useHttp(
-        useMemo(() => {
-            return {
-                url: 'https://react-complete-guide-htt-c56f1-default-rtdb.firebaseio.com/tasks.json',
-            };
-        }, []),
-        transformTasks
-    );
-
-    /*
-        fetchTasks will be different as it is a function/object, so we'll add
-        useCallback in use-https for sendRequest to prevent infinite loop
-    */
     useEffect(() => {
-        fetchTasks();
+        const transformTasks = (tasksObj) => {
+            const loadedTasks = [];
+    
+            for (const taskKey in tasksObj) {
+                loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+            }
+    
+            setTasks(loadedTasks);
+        };
+
+        fetchTasks(
+            {
+                url: 'https://react-complete-guide-htt-c56f1-default-rtdb.firebaseio.com/tasks.json',
+            },
+            transformTasks
+        );
     }, [fetchTasks]);
 
     const taskAddHandler = (task) => {
