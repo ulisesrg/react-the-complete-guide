1e95051deb4d87d8a98a909b098064ffb16cb613
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-14 14:33:24 -0500

    192-Building-a-Custom-Http-Hook

--- 11-more-realistic-custom-hooks/src/App.js
+++ 11-more-realistic-custom-hooks/src/App.js
@@ -8,33 +8,6 @@ function App() {
     const [error, setError] = useState(null);
     const [tasks, setTasks] = useState([]);
 
-    const fetchTasks = async (taskText) => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch(
-                'https://react-complete-guide-htt-c56f1-default-rtdb.firebaseio.com/tasks.json'
-            );
-
-            if (!response.ok) {
-                throw new Error('Request failed!');
-            }
-
-            const data = await response.json();
-
-            const loadedTasks = [];
-
-            for (const taskKey in data) {
-                loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-            }
-
-            setTasks(loadedTasks);
-        } catch (err) {
-            setError(err.message || 'Something went wrong!');
-        }
-        setIsLoading(false);
-    };
-
     useEffect(() => {
         fetchTasks();
     }, []);
