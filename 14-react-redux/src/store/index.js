e8437b971ea44e9838f1fd076573005f8b65ecfa
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-23 11:07:35 -0500

    242-Adding-State-Slices

--- 14-react-redux/src/store/index.js
+++ 14-react-redux/src/store/index.js
@@ -1,6 +1,34 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        /* 
+            We were told that with legacy or regular reducers (not from redux toolkit)
+            we were not allowed to modify or mutate the state object. With redux toolkit,
+            we can write instructions that "would" mutate the object but redux toolkit
+            will internally work a way to automatically create a new object, so we can
+            "mutate" the object to write shorter instructions and not to worry about
+            preventing to mutate it.
+        */
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        },
+    },
+});
 
 const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
