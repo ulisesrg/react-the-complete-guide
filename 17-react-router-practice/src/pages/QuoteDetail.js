3f575c8c4d23104d8e59f1f8bdf2e5e781bc8267
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-29 12:31:08 -0500

    289-Sending-and-Getting-Quote-Data-Via-Http

--- 17-react-router-practice/src/pages/QuoteDetail.js
+++ 17-react-router-practice/src/pages/QuoteDetail.js
@@ -1,26 +1,58 @@
+import { useEffect } from 'react';
 import { Link, Route, useRouteMatch, useParams } from 'react-router-dom';
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 
-const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
-    { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
-];
+// const DUMMY_QUOTES = [
+//     { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+//     { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+// ];
 
 const QuoteDetail = () => {
     const params = useParams();
     const match = useRouteMatch();
 
-    const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+    const { quoteId } = params;
 
-    if (!quote) {
+    const {
+        sendRequest,
+        status,
+        data: loadedQuote,
+        error,
+    } = useHttp(getSingleQuote, true);
+
+    // const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
+
+    if (status === 'pending') {
+        return (
+            <div className="centered">
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    if (error) {
+        return <p className="centered focused">{error}</p>;
+    }
+
+    if (!loadedQuote.text) {
         return <p>No quote found!</p>;
     }
 
     return (
         <>
-            <HighlightedQuote text={quote.text} author={quote.author} />
+            <HighlightedQuote
+                text={loadedQuote.text}
+                author={loadedQuote.author}
+            />
             {/* Next line renders the content if the url is not for comments */}
             {/* <Route path={`/quotes/${params.quoteId}`} exact>  IT CAN BE REPLACED BY:*/}
             <Route path={match.path} exact>
