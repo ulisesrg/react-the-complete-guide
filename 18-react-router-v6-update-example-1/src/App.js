60972e37a18a598b9ec80ecac8493ee5c78fee12
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-29 17:53:11 -0500

    Upgrade example 1 in React Router v6

--- 18-react-router-v6-update-example-1/src/App.js
+++ 18-react-router-v6-update-example-1/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -10,17 +10,18 @@ function App() {
         <div>
             <MainHeader />
             <main>
-                <Switch>
-                    <Route path="/welcome">
-                        <Welcome />
-                    </Route>
-                    <Route path="/products" exact>
-                        <Products />
-                    </Route>
-                    <Route path="/products/:productId">
-                        <ProductDetail />
-                    </Route>
-                </Switch>
+                <Routes>
+                    <Route path="/welcome" element={<Welcome />} />
+                    <Route path="/products" element={<Products />} />
+                    {/* 
+                        No more "exact" attribute, now if you want to show a component
+                        for every match for "/products" you can set the path as "/products/*"
+                    */}
+                    <Route
+                        path="/products/:productId"
+                        element={<ProductDetail />}
+                    />
+                </Routes>
             </main>
         </div>
     );
