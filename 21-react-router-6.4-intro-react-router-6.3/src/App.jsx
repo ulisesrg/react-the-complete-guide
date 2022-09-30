df40dba2a4bdf132f4bca71f342375965c923b6e
Ulises Rojas González <ulises.rogo@gmail.com>
2022-09-30 16:47:50 -0500

    Upgrade Demo Project to React Router v6.4

--- 21-react-router-6.4-intro-react-router-6.3/src/App.jsx
+++ 21-react-router-6.4-intro-react-router-6.3/src/App.jsx
@@ -1,27 +1,50 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from 'react-router-dom';
 
 import BlogLayout from './pages/BlogLayout';
-import BlogPostsPage from './pages/BlogPosts';
-import NewPostPage from './pages/NewPost';
-import PostDetailPage from './pages/PostDetail';
-import RootLayout from './components/RootLayout';
+import BlogPostsPage, { loader as blogPostsLoader } from './pages/BlogPosts';
+import ErrorPage from './pages/Error';
+import NewPostPage, { action as newPostAction } from './pages/NewPost';
+import PostDetailPage, { loader as blogPostLoader } from './pages/PostDetail';
+import RootLayout from './pages/RootLayout';
 import WelcomePage from './pages/Welcome';
 
+/* createBrowserRouter also accepts an object with the paths (check documentation) */
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        /* 
+            Every route that encounters an error while loading data through loader
+            will bubble up the error and render the ErrorPage
+        */
+        <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
+            <Route index element={<WelcomePage />} />
+            <Route path="/blog" element={<BlogLayout />}>
+                <Route
+                    index
+                    element={<BlogPostsPage />}
+                    loader={blogPostsLoader}
+                />
+                <Route
+                    path=":id"
+                    element={<PostDetailPage />}
+                    loader={blogPostLoader}
+                />
+            </Route>
+            <Route
+                path="/blog/new"
+                element={<NewPostPage />}
+                action={newPostAction}
+            />
+        </Route>
+    )
+);
+
 function App() {
-    return (
-        <BrowserRouter>
-            <RootLayout>
-                <Routes>
-                    <Route path="/" element={<WelcomePage />} />
-                    <Route path="/blog" element={<BlogLayout />}>
-                        <Route index element={<BlogPostsPage />} />
-                        <Route path=":id" element={<PostDetailPage />} />
-                    </Route>
-                    <Route path="/blog/new" element={<NewPostPage />} />
-                </Routes>
-            </RootLayout>
-        </BrowserRouter>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
--- 21-react-router-6.4-intro-react-router-6.3/src/pages/NewPost.jsx
+++ 21-react-router-6.4-intro-react-router-6.3/src/pages/NewPost.jsx
@@ -1,30 +1,17 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {
+    redirect,
+    useActionData,
+    useNavigate,
+    useNavigation,
+} from 'react-router-dom';
 
 import NewPostForm from '../components/NewPostForm';
 import { savePost } from '../util/api';
 
 function NewPostPage() {
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [error, setError] = useState();
+    const data = useActionData();
     const navigate = useNavigate();
-
-    async function submitHandler(event) {
-        event.preventDefault();
-        setIsSubmitting(true);
-        try {
-            const formData = new FormData(event.target);
-            const post = {
-                title: formData.get('title'),
-                body: formData.get('post-text'),
-            };
-            await savePost(post);
-            navigate('/');
-        } catch (err) {
-            setError(err);
-        }
-        setIsSubmitting(false);
-    }
+    const navigation = useNavigation();
 
     function cancelHandler() {
         navigate('/blog');
@@ -32,14 +19,38 @@ function NewPostPage() {
 
     return (
         <>
-            {error && <p>{error.message}</p>}
+            {data && data.status && <p>{data.message}</p>}
             <NewPostForm
                 onCancel={cancelHandler}
-                onSubmit={submitHandler}
-                submitting={isSubmitting}
+                submitting={navigation.state === 'submitting'}
             />
         </>
     );
 }
 
 export default NewPostPage;
+
+export async function action({ request }) {
+    const formData = await request.formData();
+    const post = {
+        title: formData.get('title'),
+        body: formData.get('post-text'),
+    };
+
+    try {
+        await savePost(post);
+    } catch (error) {
+        if (error.status === 422) {
+            // tbd
+            /* 
+                If we return the error instead of throwing it, we prevent the Error page 
+                from loading and we can output error information in this through
+                useActionData hook
+            */
+            return error;
+        }
+        throw error;
+    }
+
+    return redirect('/blog');
+}
