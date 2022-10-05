57e08f91a95fa5e2173468898036372e0d081122
Ulises Rojas González <ulises.rogo@gmail.com>
2022-10-05 11:01:24 -0500

    310-Redirecting-The-User

--- 24-auth-in-react-apps/src/components/Profile/ProfileForm.js
+++ 24-auth-in-react-apps/src/components/Profile/ProfileForm.js
@@ -1,9 +1,11 @@
 import { useContext, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+    const history = useHistory();
     const newPasswordInputRef = useRef();
     const authCtx = useContext(AuthContext);
 
@@ -29,6 +31,7 @@ const ProfileForm = () => {
             }
         ).then((res) => {
             // assumption: Always succeeds
+            history.replace('/');
         });
     };
 
