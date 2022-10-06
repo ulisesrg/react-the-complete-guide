f1c0390c9cc2ba9d8b5d3c7002507403b2880a44
Ulises Rojas González <ulises.rogo@gmail.com>
2022-10-06 12:32:49 -0500

    328-Linking-Between-Pages

--- 25-first-nextjs-project/pages/news/index.js
+++ 25-first-nextjs-project/pages/news/index.js
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
 function NewsPage() {
-    return <h1>The News Page</h1>;
+    return (
+        <>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-framework">
+                        NextJS Is A Great Framework
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/news/something-else">Something else</Link>
+                </li>
+            </ul>
+        </>
+    );
 }
 
 export default NewsPage;
